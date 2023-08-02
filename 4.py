4a3e2fc7746bb594e716cbb750ea1116cd822351
umedium <umedium@bk.ru>
2023-08-02 19:39:10 +0300

    new files added

---
+++ 4.py
@@ -0,0 +1,30 @@
+# import random
+
+# # names = ["Бобочон", "Суман", "Сиёвуш", "Акаи Умед", "Земфира", "Умедчон"]
+# # print(random.choice(names))
+
+# # my_random = random.randint(10, 500)
+# # print(my_random)
+
+# names = ["Бобочон", "Суман", "Сиёвуш", "Файзиддин", "Земфира", "Умедчон", "Алишер", "Акаи Умед" ]
+# print(random.choices(names,k=3))
+
+
+# # list = ["1202", "3400", "390", "1121", "3350", "1010"]
+# # intlist = []
+
+# # for i in list:
+# #     intlist.append(int(i))
+
+# # print(intlist)
+# # intlist = sorted(intlist)
+# # print(intlist)
+
+
+# # strlist = []
+# # for i in intlist:
+# #     strlist.append(str(i))
+# # print(strlist)
+
+
+
